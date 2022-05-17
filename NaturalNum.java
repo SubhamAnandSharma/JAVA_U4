4c0ff7afeffb8d8a077a27b380637e9c4f1c7ead
SUBH8425 <100857437+SUBH8425@users.noreply.github.com>
2022-05-17 22:06:42 +0530

    Add files via upload

---
+++ NaturalNum.java
@@ -0,0 +1,15 @@
+package com.masai;
+
+public class NaturalNum {
+	
+	public static void main(String[] args) {
+
+		int n = 10;
+		
+		sumNaturalNum(n);
+	}
+	
+	public static void sumNaturalNum(int n) {
+		System.out.println((n*(n+1))/2);
+	}
+}
